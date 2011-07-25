69d7b65cdb1bc76b644aa1cd2dd590a972e1aa69
Cosmin Radoi <cosmin.radoi@gmail.com>
2011-07-24 20:43:05 -0500

    better jchord test suite

--- run_all_suite.rb
+++ run_all_suite.rb
@@ -1,8 +1,28 @@
 $programs = ["barnesHut","montecarlo","em3d","junit","coref","weka","lusearch"]
-$programs_short = ["barnesHut"]
-$programs = $programs_short
 
-$programs.each do |program|
-	toexe = "ant -f ../compare/chord/main/build.xml -Dchord.work.dir="+program+" run > "+program+"/jchord_execution.txt;"
-	puts system toexe
+$programs = [ARGV[0]] if !ARGV[0].nil?
+
+puts "Evaluating: "+($programs * " ")
+
+$programs.each do |p|
+    postfix_file = File.new("chord.properties.postfix", "r")
+    prefix_file = File.new(p+"/chord.properties.prefix", "r")
+    prop_file = File.new(p+"/chord.properties","w")
+    prefix_file.each {|line| prop_file << line}
+    prop_file << "\n"
+    postfix_file.each {|line| prop_file << line}
+    prop_file.close
+end
+exit
+$programs.each do |p|
+    toexe = "ant -f ../compare/chord/main/build.xml -Dchord.work.dir="+p+" run > "+p+"/jchord_execution.txt;"
+    worked = system toexe
+    if worked
+        puts p+" SUCCESS "
+        filter_results_exe = "ruby filter_results.rb "+p+" > "+p+"/jchord_results.txt;"
+        system filter_results_exe
+        puts p+" filtered results"
+    else
+        puts p+" FAIL"
+    end
 end
