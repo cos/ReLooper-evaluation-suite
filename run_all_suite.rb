33b22e28c0fc2d4adcb41310f3349526c2ee9cb5
Cosmin Radoi <cosmin.radoi@gmail.com>
2011-07-24 20:03:10 -0500

    update run_all_suite.rb and tested it

--- run_all_suite.rb
+++ run_all_suite.rb
@@ -1,4 +1,8 @@
-["barnesHut","montecarlo"].each do |program|
+$programs = ["barnesHut","montecarlo","em3d","junit","coref","weka","lusearch"]
+$programs_short = ["barnesHut"]
+$programs = $programs_short
+
+$programs.each do |program|
 	toexe = "ant -f ../compare/chord/main/build.xml -Dchord.work.dir="+program+" run > "+program+"/jchord_execution.txt;"
 	puts system toexe
 end
